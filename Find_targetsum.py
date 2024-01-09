1f54cc73b7b4bcc786a3d1534bd921e3c79198ec
Howo0625 <95896921+Howo0625@users.noreply.github.com>
2024-01-10 00:15:35 +0800

    Update Find_targetsum.py

--- Find_targetsum.py
+++ Find_targetsum.py
@@ -1,6 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if target == nums[i] + nums[j]:
-                    return [i,j]
+        def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        
+        for i, num in enumerate(nums):
+            diff = target - num
+
+            if diff in dict:
+                return [dict[diff], i]
+
+            dict[num] = i
